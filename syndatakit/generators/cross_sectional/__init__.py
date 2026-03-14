4a1c8be892d61dc6ad6b108e143e3b3d9679dd9c
Nityahapani <118457303+Nityahapani@users.noreply.github.com>
2026-03-14 20:37:58 +0530

    Update __init__.py

--- syndatakit/generators/cross_sectional/__init__.py
+++ syndatakit/generators/cross_sectional/__init__.py
@@ -1,2 +1,11 @@
-from .gaussian_copula import GaussianCopulaGenerator
-__all__ = ["GaussianCopulaGenerator"]
+from .gaussian_copula     import GaussianCopulaGenerator
+from .dp_gaussian_copula  import DPGaussianCopulaGenerator
+
+def VineCopulaGenerator(*args, **kwargs):
+    try:
+        from .vine_copula import VineCopulaGenerator as _V
+        return _V(*args, **kwargs)
+    except ImportError:
+        raise ImportError("VineCopulaGenerator requires: pip install syndatakit[vine]")
+
+__all__ = ["GaussianCopulaGenerator", "DPGaussianCopulaGenerator", "VineCopulaGenerator"]
